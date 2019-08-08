d0a17fd2bc10f1156205b2fe05e893ae061e5b48
Clark Williams <theclarkwilliams@gmail.com>
2019-08-08 11:24:38 -0600

    added JOTD config var

--- api/server.js
+++ api/server.js
@@ -6,7 +6,10 @@ const server = express();
 server.use(express.json())
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: "it's working!" })
+    // res.status(200).json({ api: "it's working!" })
+    res.status(200).json({ 
+        jargonOfTheDay: process.env.JOTD
+     })
 });
 
 module.exports = server;
